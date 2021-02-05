bdc5ea69f8063b5b59c18d601b68df9418911fd5
sammy <o.samy39@gmail.com>
2021-02-06 03:56:21 +0530

    Commit-2

--- src/header.js
+++ src/header.js
@@ -1,9 +1,13 @@
 
 const Header = ({user}) =>{
-    const {name, status} = user
+    const {name, status, profile_pic} = user
+    console.log(profile_pic)
     return(
         <header className="Header">
-            <h1 className="header__name">{name}</h1>
+            <div className='header__top'>
+                <h1 className="header__name">{name}</h1>
+                <img src={profile_pic} alt={name} className="header__img" />
+            </div>
             <p className="header__status">{status}</p>
         </header>
     )
